0536f9a56f79436186c7042e21afb318b257d504
Dmytro <dentistko@ukr.net>
2024-03-17 18:28:44 +0200

    1 full...

---
+++ HW_3_1_3/Program.cs
@@ -0,0 +1,46 @@
+﻿//Використовуючи Visual Studio, створіть проект за шаблоном Console Application.
+//Потрібно: Створити класи Point та Figure. Клас Point повинен містити два цілих поля і одне рядкове поле.
+//Створити три властивості одним методом доступу get. Створити власний конструктор, у тілі якого проініціалізуйте поля значеннями аргументів.
+//Клас Figure повинен містити конструктори, які приймають від 3 до 5 аргументів типу Point.
+//Створити два методи: double LengthSide(Point A, Point B), що розраховує довжину сторони багатокутника; void PerimeterCalculator(), що розраховує периметр багатокутника.
+//Написати програму, яка виводить на екран назву та периметр багатокутника.
+
+
+class Point
+{
+    public int X { get; }
+    public int Y { get; }
+    public string Field3 { get; }
+
+    public Point(int x, int y, string field3) //Конструктор в якому ініціалізую дані
+    {
+        Console.WriteLine("Enter field1");
+        var input1 = Console.ReadLine();
+        int.TryParse(input1, out x );
+        {
+            X = x;
+        }
+        
+        Console.WriteLine("Enter field2");
+        var input2 = Console.ReadLine();
+        int.TryParse(input2, out y );
+        {
+            Y = y;
+        }
+        Console.WriteLine("Enter field3");
+        Field3 = Console.ReadLine();
+        
+    }
+}
+
+
+
+
+class Figure
+{
+    public Figure(int x, int y)
+    {
+        
+    }
+    
+}
---
+++ HW_3_1_4/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+public class Point
+{
+    public int X { get; }
+    public int Y { get; }
+    public string Label { get; }
+
+    // Конструктор класу Point
+    public Point(int x, int y, string label)
+    {
+        X = x;
+        Y = y;
+        Label = label;
+    }
+}
+
+public class Figure
+{
+    private Point[] points;
+
+    // Конструктор для фігур з трьох точок
+    public Figure(Point point1, Point point2, Point point3)
+    {
+        points = new Point[] { point1, point2, point3 };
+    }
+    // Конструктор для фігур з чотирьох точок
+    public Figure(Point point1, Point point2, Point point3, Point point4)
+    {
+        points = new Point[] { point1, point2, point3, point4 };
+    }
+    // Конструктор для фігур з п'яти точок
+    public Figure(Point point1, Point point2, Point point3, Point point4, Point point5)
+    {
+        points = new Point[] { point1, point2, point3, point4, point5 };
+    }
+    // Метод для розрахунку довжини сторони багатокутника
+    private double LengthSide(Point A, Point B)
+    {
+        return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+    }
+    // Метод для розрахунку периметра багатокутника
+    public void PerimeterCalculator()
+    {
+        double perimeter = 0;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            int nextIndex = (i + 1) % points.Length;
+            perimeter += LengthSide(points[i], points[nextIndex]);
+        }
+        Console.WriteLine($"Назва багатокутника: {points.Length}-кут");
+        Console.WriteLine($"Периметр багатокутника: {perimeter}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Створення об'єкта класу Figure з використанням конструктора
+        Point pointA = new Point(0, 0, "A");
+        Point pointB = new Point(1, 0, "B");
+        Point pointC = new Point(0, 1, "C");
+        Point pointD = new Point(0, 1, "D");
+
+        Figure triangle = new Figure(pointA, pointB, pointC, pointD);
+
+        // Виведення інформації про фігуру
+        triangle.PerimeterCalculator();
+    }
+}
